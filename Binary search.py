539395b38174933ce2a79a787f8b5a68c1a47bf5
SANTHOSH V <115791144+santhoshvaradhan@users.noreply.github.com>
2022-12-16 21:54:03 +0530

    Create Binary search.py
    Binary search using list in python

---
+++ Binary search.py
@@ -0,0 +1,28 @@
+# program for linear search
+# n is number of elements
+n=int(input("Enter the number of elements: "))
+list1=[]
+for i in range(n):# loop used get elements for user
+    print("enter the element",i+1,":")
+    a=input()
+    # append function will add elements in list1 it is list
+    list1.append(a)
+# key is element which we going to search
+key=input("Enter the value to find:")
+# low is starting of index position of list is 0
+# high is last index position of list is n-1
+# found is to check element is found then the value is 1 otherwise 0
+low,high,found=0,n-1,0
+while(low>high or high<-1):
+    mid=(low+high)/2
+    if list1[mid]==key:
+        found=1
+        break
+    elif list1[mid]>key:
+        high=mid-1
+    else:
+        low=mid+1
+if found==1:
+    print("element is ssuccessfully founded")
+else:
+    print("element is not successfully founded")
